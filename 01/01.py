95117dea9dc462a6e07ded5ca12c25ce7cc76205
Martin Sonnberger <msonnberger.mmt-b2020@fh-salzburg.ac.at>
2021-12-01 08:35:53 +0100

    day 01 nicer python solution

--- 01/01.py
+++ 01/01.py
@@ -1,27 +1,13 @@
 from aocd import numbers, submit
 
 def part1():
-    count = 0
-
-    for i in range(len(numbers) - 1):
-        if numbers[i + 1] > numbers[i]:
-            count += 1
-
+    count = sum(x < y for x, y in zip(numbers, numbers[1:]))
     print(f'part 1: {count}')
     submit(count)
 
 
 def part2():
-    count = 0
-    window_size = 3
-
-    for i in range(len(numbers) - window_size):
-        sum1 = sum(numbers[i : i + window_size])
-        sum2 = sum(numbers[i + 1 : i + 1 + window_size])
-
-        if sum2 > sum1:
-            count += 1
-
+    count = sum(x < y for x, y in zip(numbers, numbers[3:]))
     print(f'part 2: {count}')
     submit(count)
 
