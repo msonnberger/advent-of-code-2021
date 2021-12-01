1372c98103227e7f4d4ceefba77757f033da55d2
Martin Sonnberger <msonnberger.mmt-b2020@fh-salzburg.ac.at>
2021-12-01 10:25:25 +0100

    comment out submit

--- 01/01.py
+++ 01/01.py
@@ -3,13 +3,13 @@ from aocd import numbers, submit
 def part1():
     count = sum(x < y for x, y in zip(numbers, numbers[1:]))
     print(f'part 1: {count}')
-    submit(count)
+    #submit(count)
 
 
 def part2():
     count = sum(x < y for x, y in zip(numbers, numbers[3:]))
     print(f'part 2: {count}')
-    submit(count)
+    #submit(count)
 
 part1()
 part2()
