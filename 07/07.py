350121d44a373fa7d000ff91beb3f46f67a730dd
Martin Sonnberger <msonnberger.mmt-b2020@fh-salzburg.ac.at>
2021-12-07 09:24:54 +0100

    use median instead of brute force

--- 07/07.py
+++ 07/07.py
@@ -1,24 +1,23 @@
 from aocd import data
+from statistics import median
 
-def faster_sum(n):
+positions = [int(x) for x in data.split(',')]
+
+def n_sum(n):
     return (n * n + n) // 2
 
-def solve(part2 = False):
-    positions = [int(x) for x in data.split(',')]
+def part2():
     min_cost = 1e12
 
     for i in range(min(positions), max(positions) + 1):
-        total_cost = 0
-        for pos in positions:
-            if not part2:
-                total_cost += abs(pos - i)
-            else:
-                total_cost += faster_sum(abs(pos - i))
-
+        total_cost = sum([n_sum(abs(pos - i)) for pos in positions])
         min_cost = min(min_cost, total_cost)
 
-    part = 2 if part2 else 1
-    print(f"part {part}: {min_cost}")
+    print(f"part 2: {min_cost}")
 
-solve()
-solve(part2=True)
+def part1():
+    result = int(sum([abs(pos - median(positions)) for pos in positions]))
+    print(f"part 1: {result}")
+
+part1()
+part2()
