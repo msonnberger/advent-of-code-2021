72c0a29570a6fd1e1d7f7f4f44e83d2041cbf7ab
Martin Sonnberger <msonnberger.mmt-b2020@fh-salzburg.ac.at>
2021-12-08 19:49:13 +0100

    day 08 part 2

--- 08/08.py
+++ 08/08.py
@@ -1,16 +1,55 @@
 from aocd import lines
 
-def solve():
-    digits = [""] * 10
-    print(digits)
-
+def part1():
     count = 0
     for line in lines:
-        inp, out = line.split(" | ")
+        out = line.split(" | ")
         for o in out.split():
             if len(o) in [2, 3, 4, 7]:
                 count += 1
 
     print(f"part 1: {count}")
 
-solve()
+def part2():
+    total = 0
+
+    for line in lines:
+        digits = ["null"] * 10
+        inp, out = line.split(" | ")
+        for i in line.replace(" | ", " ").split():
+            if len(i) == 2:
+                digits[1] = i
+            elif len(i) == 3:
+                digits[7] = i
+            elif len(i) == 4:
+                digits[4] = i
+            elif len(i) == 7:
+                digits[8] = i
+
+        for i in line.replace(" | ", " ").split():
+            if len(i) == 6:
+                if (set(digits[4]).issubset(i)):
+                    digits[9] = i
+                elif (set(digits[7]).issubset(i)):
+                    digits[0] = i
+                else:
+                    digits[6] = i
+            elif len(i) == 5:
+                if (set(digits[7]).issubset(i)):
+                    digits[3] = i
+                elif len(set(digits[4]).intersection(set(i))) == 3:
+                    digits[5] = i
+                else:
+                    digits[2] = i
+
+        digit_str = ""
+        digits = [set(x) for x in digits]
+        for o in out.split():
+            digit_str += str(digits.index(set(o)))
+
+        total += int(digit_str)
+
+    print(f"part 2: {total}")
+
+part1()
+part2()
