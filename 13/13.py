85c98dd7bd0ec385647ca519156b67f076010f1f
Martin Sonnberger <msonnberger.mmt-b2020@fh-salzburg.ac.at>
2021-12-13 10:02:33 +0100

    use set instead of dict

--- 13/13.py
+++ 13/13.py
@@ -1,54 +1,51 @@
 from aocd import data
-from collections import defaultdict
-import numpy as np
 
 points, folds = data.split('\n\n')
-
 points = [line.split(',') for line in points.splitlines()]
 points = [(int(x), int(y)) for x, y in points]
 folds = [line.split()[-1].split('=') for line in folds.splitlines()]
 folds = [[axis, int(val)] for axis, val in folds]
-paper = defaultdict(int)
+paper = set()
 
 for point in points:
-    paper[point] = 1
+    paper.add(point)
 
 
 def part1():
     axis, val = folds[0]
     fold(axis, val)
-    print(f"part 1: {sum(paper.values())}")
+    print(f"part 1: {len(paper)}")
 
 def part2():
     for axis, val in folds:
         fold(axis, val)
 
-    max_row = max(x for x, _ in paper.keys())
-    max_col = max(y for _, y in paper.keys())
+    max_row = max(x for x, _ in paper)
+    min_row = min(x for x, _ in paper)
+    max_col = max(y for _, y in paper)
+    min_col = min(y for _, y in paper)
 
-    matrix = np.ones((max_row + 1, max_col + 1))
-
-    for point in paper.keys():
-        matrix[point] = 8
-
-    print(matrix.transpose())
+    print("part 2:")
+    for y in range(min_col, max_col + 1):
+        line = ""
+        for x in range(min_row, max_row + 1):
+            line += "#" if (x, y) in paper else " "
+        
+        print(line)
 
 def fold(axis, val):
-    idx = 0 if axis == 'x' else 1
-    point_change = [point for point in paper.keys() if point[idx] > val]
+    global paper
+    folded = set()
 
-    for point in point_change:
-        del paper[point]
-        d = point[idx] - val
-        x, y = point
-        if axis == 'x':
-            x = x - 2 * d
-        
-        if axis == 'y':
-            y = y - 2 * d
+    for x, y in paper:
+        if axis == 'x' and x > val:
+            folded.add((2 * val - x, y))
+        elif axis == 'y' and y > val:
+            folded.add((x, 2 * val - y))
+        else:
+            folded.add((x, y))
 
-        new_point = (x, y)
-        paper[new_point] = 1
+    paper = folded
 
 part1()
 part2()
